e41f7fa68c29c29c7779f9ef4efa1da3d6b7c911
MilaGrishkova <49581816+MilaGrishkova@users.noreply.github.com>
2021-03-01 13:57:10 +0100

    Create Sound.js

---
+++ Sound.js
@@ -0,0 +1,190 @@
+{
+	"sound.mage_m.ability": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Mage(m:f)/mage_m.ability.wav",
+		"type":"audio"
+	},
+	"sound.mage_m.hurt": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Hurt/sound.m.hurt1.wav",
+		"type":"audio"
+	},
+	"sound.mage_f.ability": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Mage(m:f)/mage_f.ability.wav",
+		"type":"audio"
+	},
+	"sound.mage_f.hurt": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Hurt/sound.f.hurt1.wav",
+		"type":"audio"
+	},
+	"sound.archer_m.ability": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Archer(m:f)/sound.archer_m.ability.wav",
+		"type":"audio"
+	},
+	"sound.archer_m.hurt": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Hurt/sound.m.hurt2.wav",
+		"type":"audio"
+	},
+	"sound.archer_f.ability": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Archer(m:f)/sound.archer_m.ability.wav",
+		"type":"audio"
+	},
+	"sound.archer_f.hurt": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Hurt/sound.f.hurt2.wav",
+		"type":"audio"
+	},
+	"sound.fighter_m.ability": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/fighter(m:f)/sound.fighter_m.ability.wav",
+		"type":"audio"
+	},
+	"sound.fighter_m.hurt": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Hurt/sound.m.hurt1.wav",
+		"type":"audio"
+	},
+	"sound.fighter_f.ability": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/fighter(m:f)/sound.fighter_f.ability.wav",
+		"type":"audio"
+	},
+	"sound.fighter_f.hurt": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Hurt/sound.f.hurt3.wav",
+		"type":"audio"
+	},
+	"sound.healer_m.ability": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Healer/sound.healer_m.ability.wav",
+		"type":"audio"
+	},
+	"sound.healer_m.hurt": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Hurt/sound.m.hurt1.wav",
+		"type":"audio"
+	},
+	"sound.healer_f.ability": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Healer/sound.healer_m.ability.wav",
+		"type":"audio"
+	},
+	"sound.healer_f.hurt": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Hurt/sound.f.hurt4.wav",
+		"type":"audio"
+	},
+	"sound.troubadour_m.ability": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Buff/sound.troubadour_m.ability.wav",
+		"type":"audio"
+	},
+	"sound.troubadour_m.hurt": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Hurt/sound.m.hurt4.wav",
+		"type":"audio"
+	},
+	"sound.troubadour_f.ability": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Buff/sound.troubadour_m.ability.wav",
+		"type":"audio"
+	},
+	"sound.troubadour_f.hurt": {
+		"path":"data/sounds/effectsv/Mila/MilaBearbeitung/Hurt/sound.f.hurt1.wav",
+		"type":"audio"
+	},
+	"sound.earth_elemental_m.ability": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Earth/sound.earth_elemental_m.ability.wav",
+		"type":"audio"
+	},
+	"sound.earth_elemental_m.hurt": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Hurt/sound.m.hurt3.wav",
+		"type":"audio"
+	},
+	"sound.ability.magic_fire_0.impact": {
+		"path":"",
+		"type":"audio"
+	},
+	"sound.ability.magic_fire_0.projectile": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Mage(m:f)/ability.magic_fire_0.projectile.wav",
+		"type":"audio"
+	},
+	"sound.ability.magic_fire_1.impact": {
+		"path":"",
+		"type":"audio"
+	},
+	"sound.ability.magic_fire_1.projectile": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Mage(m:f)/ability.magic_fire_0.projectile.wav",
+		"type":"audio"
+	},
+	"sound.ability.magic_frost_0.impact": {
+		"path":"",
+		"type":"audio"
+	},
+	"sound.ability.magic_frost_0.projectile": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Mage(m:f)/sound.ability.magic_frost_0.projectile.wav",
+		"type":"audio"
+	},
+	"sound.ability.magic_frost_1.impact": {
+		"path":"",
+		"type":"audio"
+	},
+	"sound.ability.magic_frost_1.projectile": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Mage(m:f)/sound.ability.magic_frost_0.projectile.wav",
+		"type":"audio"
+	},
+	"sound.ability.buff_0.impact": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Buff/sound.ability.buff_0.impact.wav",
+		"type":"audio"
+	},
+	"sound.ability.buff_0.projectile": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Buff/sound.ability.buff_0.projectile.wav",
+		"type":"audio"
+	},
+	"sound.ability.earth_arrow_0.impact": {
+		"path":"",
+		"type":"audio"
+	},
+	"sound.ability.earth_arrow_0.projectile": {
+		"path":"",
+		"type":"audio"
+	},
+	"sound.ability.melee.impact": {
+		"path":"",
+		"type":"audio"
+	},
+	"sound.ability.bow.impact": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Archer(m:f)/ArrowHit3.wav",
+		"type":"audio"
+	},
+	"sound.ability.bow.projectile": {
+		"path":"",
+		"type":"audio"
+	},
+	"sound.ability.healing_0.impact": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Healer/sound.ability.healing_0.impact.wav",
+		"type":"audio"
+	},
+	"sound.ability.healing_0.projectile": {
+		"path":"data/sounds/effects/Mila/MilaBearbeitung/Healer/sound.ability.healing_0.projectile.wav",
+		"type":"audio"
+	},
+	"sound.power-up.troubadour.0": {
+		"path": "data/sounds/effects/Mila/2020_10_27/sound.power-up.troubadour.0.wav",
+		"type": "audio"
+	},
+	"sound.effects.trail-particles-up-0": {
+		"path": "data/sounds/effects/Mila/MilaBearbeitung/Soundeffect/sound.effects.trail-particles-up-0.wav",
+		"type": "audio"
+	},
+	"sound.effects.flash-buff-defense-up-0": {
+		"path": "data/sounds/effects/Mila/MilaBearbeitung/Soundeffect/sound.effects.flash-buff-defense-up-0.wav",
+		"type": "audio"
+	},
+	"sound.events.unit-death.generic": {
+		"path": "data/sounds/effects/Mila/MilaBearbeitung/Dust/sound.events.unit-death.generic.wav",
+		"type": "audio"
+	},
+	"sound.effects.ground-tile-contour-flash-1": {
+		"path": "data/sounds/effects/Mila/MilaBearbeitung/Soundeffect/sound.effects.ground-tile-contour-flash-1.wav",
+		"type": "audio"
+	},
+	"sound.ui.radial-menu.disappear.no-choice": {
+		"path": "data/sounds/effects/Mila/MilaBearbeitung/Soundeffect/sound.ui.radial-menu.disappear.no-choice.wav",
+		"type": "audio"
+	},
+	"sound.ui.radial-menu.appear": {
+		"path": "data/sounds/effects/Mila/Mila/MilaBearbeitung/Soundeffect/sound.ui.radial-menu.appear.wav",
+		"type": "audio"
+	},
+	"sound.ui.combat.inspect.appear": {
+		"path": "data/sounds/effects/Mila/Mila/MilaBearbeitung/Soundeffect/sound.ui.combat.inspect.appear.wav",
+		"type": "audio"
+	}
+}
